64770aaa4e1eee086736317bb406059ffeb8aae1
Andrii Melnyk <lethoss@ukr.net>
2020-07-01 14:52:38 +0300

    adding my files to my repository

---
+++ vue-gallery/src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+  <h2>Home page</h2>
+  <router-link to="/Gallery">Gallery </router-link>
+  <router-link to="/About">About </router-link>
+  <div>This is my built with Vue.js gallery page. The images are taken from Unsplash API.</div>
+  <div>Other my works you can see on Github Pages, the link is on "About" page.</div>
+  </div>
+</template>
