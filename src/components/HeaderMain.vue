9be21c00e43a2b156e29b0980afa253746c9f5f1
Andrii Melnyk <lethoss@ukr.net>
2020-07-16 18:29:25 +0300

    repair auth

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,31 +1,31 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <router-link to="/" exact class="nav-link">Home<span class="sr-only">(current)</span></router-link>
-      </li>
-      <li class="nav-item active">
-        <router-link to="/gallery" exact class="nav-link">Gallery<span class="sr-only">(current)</span></router-link>
-      </li>
-      <li class="nav-item active">
-        <router-link to="/about" exact class="nav-link">About<span class="sr-only">(current)</span></router-link>
-      </li>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link to="/" exact class="nav-link">Home<span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="/gallery" exact class="nav-link">Gallery<span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item active">
+          <router-link to="/about" exact class="nav-link">About<span class="sr-only">(current)</span></router-link>
+        </li>
 
-    </ul>
-  </div>
-  <router-link to="/login" exact class="nav-link">Login<span class="sr-only">(current)</span></router-link>
-  <router-link to="/register" exact class="nav-link">Register<span class="sr-only">(current)</span></router-link>
-  <span class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      {{this.userName.name || 'User Without Name'}}
-    </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-      <a class="dropdown-item" href="#">Clear saved images</a>
-      <router-link to="/login" class="dropdown-item" @click="logout">Logout</router-link>
+      </ul>
     </div>
-  </span>
-</nav>
+    <router-link to="/login" exact class="nav-link">Login<span class="sr-only">(current)</span></router-link>
+    <router-link to="/register" exact class="nav-link">Register<span class="sr-only">(current)</span></router-link>
+    <span class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {{this.userName.name || 'User Without Name'}}
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        <a class="dropdown-item" href="#" >Clear saved images</a>
+        <a @click="logout" class="dropdown-item">Logout</a>
+      </div>
+    </span>
+  </nav>
 </template>
 
 <script>
@@ -41,6 +41,7 @@ export default {
     async logout() {
       await firebase.auth().signOut()
       this.userName = {}
+      this.$router.push('/login')
     },
   },
   async mounted () {
@@ -49,9 +50,10 @@ export default {
         this.userName = (await firebase.database().ref(`/users/${uid}/info`).once('value')).val()
       } catch (e) {
         console.log(e)
+        this.$router.push('/login')
         throw e
       }
-    },
+  },
 }
 </script>
 
