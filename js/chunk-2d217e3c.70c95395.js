d369a4e80ad72a81b400dbf89406cafa98ffaa8f
Andrii Melnyk <lethoss@ukr.net>
2020-07-16 18:28:52 +0300

    deploy

---
+++ js/chunk-2d217e3c.70c95395.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d217e3c"],{c93c:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" Name - Andrey, email - lethoss@urk.net ")])},l=[],u=t("2877"),r={},s=Object(u["a"])(r,c,l,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d217e3c.70c95395.js.map
---
+++ js/chunk-2d22996d.b8826c6e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22996d"],{dda8:function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",[n("h2",[e._v("Error page")]),n("router-link",{attrs:{to:"/",exact:""}},[e._v(" Back to Home page ")])],1)},a=[],o=n("2877"),c={},u=Object(o["a"])(c,r,a,!1,null,null,null);t["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d22996d.b8826c6e.js.map
